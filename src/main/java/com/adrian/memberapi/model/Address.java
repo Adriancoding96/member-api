c15b9368dd1e55736285ad505254be9be577e7d7
Adriancoding96 <adrian.nilsson.coding@gmail.com>
2023-11-03 10:10:02 +0100

    test

---
+++ src/main/java/com/adrian/memberapi/model/Address.java
@@ -0,0 +1,89 @@
+package com.adrian.memberapi.model;
+
+import jakarta.persistence.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "address")
+public class Address {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @Column(name = "street")
+    private String street;
+    @Column(name = "postal_code")
+    private String postalCode;
+    @Column(name = "city")
+    private String city;
+
+    //Anvöäd set istället för list för att förhindra att samma member mappas till samma address mer en en gång
+    @OneToMany(mappedBy = "address", fetch = FetchType.LAZY)
+    private Set<Member> members = new HashSet<>();
+
+    public Address() {
+    }
+
+    public Address(String street, String postalCode, String city) {
+        this.street = street;
+        this.postalCode = postalCode;
+        this.city = city;
+    }
+
+    public Address(String street, String postalCode, String city, Set<Member> members) {
+        this.street = street;
+        this.postalCode = postalCode;
+        this.city = city;
+        this.members = members;
+    }
+
+    public Address(long id, String street, String postalCode, String city, Set<Member> members) {
+        this.id = id;
+        this.street = street;
+        this.postalCode = postalCode;
+        this.city = city;
+        this.members = members;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public Set<Member> getMembers() {
+        return members;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public void setMembers(Set<Member> members) {
+        this.members = members;
+    }
+}
---
+++ src/main/java/com/adrian/memberapi/model/Member.java
@@ -0,0 +1,107 @@
+package com.adrian.memberapi.model;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "member")
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "first_name")
+    private String firstName;
+    @Column(name = "last_name")
+    private String lastName;
+    @Column(name = "email")
+    private String email;
+    @Column(name = "phone")
+    private String phone;
+    @Column(name = "date_of_birth")
+    private LocalDate dateOfBirth;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "address_id")
+    private Address address;
+
+    public Member() {
+    }
+
+    public Member(String firstName, String lastName, String email, String phone, LocalDate dateOfBirth, Address address) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.phone = phone;
+        this.dateOfBirth = dateOfBirth;
+        this.address = address;
+    }
+
+    public Member(long id, String firstName, String lastName, String email, String phone, LocalDate dateOfBirth, Address address) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.phone = phone;
+        this.dateOfBirth = dateOfBirth;
+        this.address = address;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public LocalDate getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public void setDateOfBirth(LocalDate dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+}
